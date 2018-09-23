c5f6cdef81937aad18cfc775c8ff57489f3608dd
Dhinesh <dhinesh.rs@gmail.com>
2018-09-23 15:22:04 +0530

    code fine tune

--- src/main/java/com/example/springbootdemos/controller/GreetingController.java
+++ src/main/java/com/example/springbootdemos/controller/GreetingController.java
@@ -2,7 +2,9 @@ package com.example.springbootdemos.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
