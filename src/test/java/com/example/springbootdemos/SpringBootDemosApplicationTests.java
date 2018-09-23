5295bc567b139911975f38168fe729e745d3794b
Dhinesh <dhinesh.rs@gmail.com>
2018-09-23 14:38:36 +0530

    First Commit

---
+++ src/test/java/com/example/springbootdemos/SpringBootDemosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootdemos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
